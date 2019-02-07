ad71fd848b6ab381c2d589d14c8fe9438badfee5
Lensual <lenvisual@gmail.com>
2019-02-08 00:52:57 +0800

    实现IDisposable

--- PacketBaseLib/PacketBase.cs
+++ PacketBaseLib/PacketBase.cs
@@ -447,17 +447,25 @@ namespace PacketBaseLib
                 }
 
                 // TODO: 释放未托管的资源(未托管的对象)并在以下内容中替代终结器。
+                if (this.RawPtr != IntPtr.Zero)
+                {
+                    Marshal.FreeHGlobal(this.RawPtr);
+                    this.RawPtr = IntPtr.Zero;
+                }
+
                 // TODO: 将大型字段设置为 null。
+                fields = null;
 
                 disposedValue = true;
             }
         }
 
         // TODO: 仅当以上 Dispose(bool disposing) 拥有用于释放未托管资源的代码时才替代终结器。
-        // ~PacketBase() {
-        //   // 请勿更改此代码。将清理代码放入以上 Dispose(bool disposing) 中。
-        //   Dispose(false);
-        // }
+        ~PacketBase()
+        {
+            // 请勿更改此代码。将清理代码放入以上 Dispose(bool disposing) 中。
+            Dispose(false);
+        }
 
         // 添加此代码以正确实现可处置模式。
         public void Dispose()
@@ -465,7 +473,7 @@ namespace PacketBaseLib
             // 请勿更改此代码。将清理代码放入以上 Dispose(bool disposing) 中。
             Dispose(true);
             // TODO: 如果在以上内容中替代了终结器，则取消注释以下行。
-            // GC.SuppressFinalize(this);
+            GC.SuppressFinalize(this);
         }
 
 
